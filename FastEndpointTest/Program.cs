5ddbafc84282101861fb82f70f5b8eadcf8287c3
Kevin Venclovas <k.venclovas@alfa-direkt.de>
2024-06-03 10:31:21 +0200

    update

--- FastEndpointTest/Program.cs
+++ FastEndpointTest/Program.cs
@@ -1,101 +1,101 @@
 using FastEndpoints;
-using FastEndpoints.Security;
-using FastEndpoints.Swagger;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
+using FastEndpointTest;
+using Microsoft.AspNetCore.HttpLogging;
 using Newtonsoft.Json;
-using Newtonsoft.Json.Converters;
-using System.Data;
+using Newtonsoft.Json.Serialization;
 
-namespace FastEndpointTest
+try
 {
-    public class Program
+
+    var builder = WebApplication.CreateBuilder(args);
+
+    builder.Logging.ClearProviders();
+
+    // SetUp FastEndpoints
+    builder.Services.SetUpFastEndpoints();
+
+    // Configurate Http logging
+    builder.Services.AddHttpLogging(logging =>
     {
-        public static void Main(string[] args)
+        logging.LoggingFields =
+            HttpLoggingFields.RequestMethod |
+            HttpLoggingFields.RequestPath |
+            HttpLoggingFields.RequestBody |
+            HttpLoggingFields.ResponseBody |
+            HttpLoggingFields.ResponseStatusCode;
+        logging.RequestBodyLogLimit = 4096;
+        logging.ResponseBodyLogLimit = 4096;
+        logging.CombineLogs = true;
+    });
+
+    // Add cors settings
+    builder.Services.SetUpCors();
+
+    // Add services to the container.
+    builder.Services.AddControllers();
+
+    // SetUp authentication
+    builder.Services.SetUpAuthentication();
+
+    // Add middleware
+    builder.Services.AddMvc();
+
+    var app = builder.Build();
+
+    // Add Swagger UI
+#if !RELEASE
+
+    app.UseOpenApi();
+
+    app.UseSwaggerUi(c =>
+    {
+        c.Path = string.Empty;
+        c.DocumentTitle = "AlfaTool API";
+    });
+
+#endif
+
+    app.UseFastEndpoints(c =>
+    {
+        c.Serializer.RequestDeserializer = async (req, tDto, jCtx, ct) =>
         {
-            var builder = WebApplication.CreateBuilder(args);
-            builder.Services.AddFastEndpoints();
+            using var reader = new StreamReader(req.Body);
+            return JsonConvert.DeserializeObject(await reader.ReadToEndAsync(), tDto);
+        };
+        c.Serializer.ResponseSerializer = (rsp, dto, cType, jCtx, ct) =>
+        {
+            rsp.ContentType = cType;
+            return rsp.WriteAsync(JsonConvert.SerializeObject(dto), ct);
+        };
+        c.Endpoints.RoutePrefix = "api";
+        c.Endpoints.ShortNames = true;
+    });
 
-            builder.Services.SwaggerDocument(o =>
-            {
-                o.DocumentSettings = s =>
-                {
-                    s.DocumentName = "v1";
-                    s.Title = "Alfa Tools";
-                    s.Version = "v1";
-                };
-                o.ExcludeNonFastEndpoints = true;
-                o.EnableJWTBearerAuth = true;
-                o.AutoTagPathSegmentIndex = 0;
-                o.ShortSchemaNames = true;
-                o.TagDescriptions = t =>
-                {
-                    t["Authentication"] = "All endpoints to our dear users <3";
-                };
-                o.NewtonsoftSettings = s =>
-                {
-                    s.Converters.Add(new StringEnumConverter());
-                };
-            });
+    app.UseHttpLogging();
 
-            builder.Services.AddAuthentication(x =>
-            {
-                x.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
-                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-            });
+    // Configure the HTTP request pipeline.
+    app.UseHttpsRedirection();
+    app.UseRouting();
 
-            builder.Services.AddJWTBearerAuth("Bx65sP54FEXU40J22VOF6hrPEYT1suy5wnz73aGXwLV7UQHKl8QtkSDRhsvMdbh");
+    app.UseCors();
+    app.UseAuthentication();
+    app.UseAuthorization();
 
-            builder.Services.AddAuthorization();
-
-            builder.Services.AddMvc();
-
-            // Add services to the container.
-
-            builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-
-            var app = builder.Build();
-             // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseOpenApi();
-                app.UseSwaggerUi(c =>
-                {
-                    c.Path = string.Empty;
-                    c.DocumentTitle = "AlfaTool API";
-                });
-            }
-
-            app.UseFastEndpoints(c =>
-            {
-                c.Serializer.RequestDeserializer = async (req, tDto, jCtx, ct) =>
-                {
-                    using var reader = new StreamReader(req.Body);
-                    return JsonConvert.DeserializeObject(await reader.ReadToEndAsync(), tDto);
-                };
-                c.Serializer.ResponseSerializer = (rsp, dto, cType, jCtx, ct) =>
-                {
-                    rsp.ContentType = cType;
-                    return rsp.WriteAsync(JsonConvert.SerializeObject(dto), ct);
-                };
-                c.Endpoints.RoutePrefix = "api";
-              
-                c.Endpoints.ShortNames = true;
-            });
-
-            app.UseHttpLogging();
-
-            app.UseHttpsRedirection();
-            app.UseRouting();
-
-            app.UseCors();
-            app.UseAuthentication();
-            app.UseAuthorization();
-            app.UseResponseCaching();
-
-            app.Run();
+    JsonConvert.DefaultSettings = () => new JsonSerializerSettings
+    {
+        Formatting = Formatting.Indented,
+        ReferenceLoopHandling = ReferenceLoopHandling.Ignore,
+        ContractResolver = new DefaultContractResolver
+        {
+            NamingStrategy = new CamelCaseNamingStrategy()
         }
-    }
+    };
+
+    app.UseResponseCaching();
+    app.Run();
+
+}
+catch (Exception exception)
+{
+    throw;
 }
---
+++ FastEndpointTest/StartUpExtension.cs
@@ -0,0 +1,102 @@
+﻿using FastEndpoints;
+using FastEndpoints.Security;
+using FastEndpoints.Swagger;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Newtonsoft.Json.Converters;
+
+namespace FastEndpointTest
+{
+    public static class StartUpExtension
+    {
+        public static void SetUpCors(this IServiceCollection collectionService)
+        {
+            collectionService.AddCors(options =>
+            {
+                // TODO Setup for Release
+                options.AddPolicy(name: "All",
+                                    builder =>
+                                    {
+                                        builder.AllowAnyOrigin();
+                                        builder.AllowAnyMethod();
+                                        builder.AllowAnyHeader();
+                                    });
+
+                options.AddPolicy(name: "WebhookSendInBlue",
+                                 builder =>
+                                 {
+                                     builder.AllowAnyOrigin();
+                                     builder.AllowAnyMethod();
+                                     builder.AllowAnyHeader();
+                                 });
+
+                options.AddPolicy(name: "Intern",
+                                  builder =>
+                                  {
+                                      builder.AllowAnyOrigin();
+                                      builder.AllowAnyMethod();
+                                      builder.AllowAnyHeader();
+                                  });
+
+            });
+        }
+    
+        public static void SetUpFastEndpoints(this IServiceCollection collectionService) 
+        {
+            collectionService.AddFastEndpoints
+            (
+                o =>
+                {
+                    o.IncludeAbstractValidators = true;
+                }
+            );
+
+            // Add reponse caching
+            collectionService.AddResponseCaching();
+
+            // Add Swagger
+            collectionService.SwaggerDocument(o =>
+            {
+                o.DocumentSettings = s =>
+                {
+                    s.DocumentName = "v1";
+                    s.Title = "Alfa Tools";
+                    s.Version = "v1";
+                };
+                o.ExcludeNonFastEndpoints = true;
+                o.EnableJWTBearerAuth = true;
+                o.AutoTagPathSegmentIndex = 0;
+                o.ShortSchemaNames = true;
+                o.TagDescriptions = t =>
+                {
+                    t["Authentication"] = "All endpoints to our dear users <3";
+                    t["Health"] = "All endpoints about application health";
+                    t["Application"] = "All endpoints about application informations";
+                    t["User"] = "All endpoints about application users";
+                };
+                o.NewtonsoftSettings = s =>
+                {
+                    s.Converters.Add(new StringEnumConverter());
+                };
+            });
+
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            collectionService.AddEndpointsApiExplorer();
+        }
+    
+        public static void SetUpAuthentication(this IServiceCollection collectionService)
+        {
+            // Add authentication
+            collectionService.AddAuthentication(x =>
+            {
+                x.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            });
+
+            collectionService.AddJWTBearerAuth("Bx65sP54FEXU40J22VOF6hrPEYT1suy5wnz73aGXwLV7UQHKl8QtkSDRhsvMdbh");
+
+            collectionService.AddAuthorization();
+        }
+
+    }
+}
